6eb596db52d53f06bd6d51f928fc720a543bb74f
Jumperreal <167784477+Jumperreal@users.noreply.github.com>
2024-09-25 17:28:41 +0200

    Guessing game.py

--- Guessing game.py
+++ Guessing game.py
@@ -2,7 +2,7 @@ from random import randint
 print('I came up with a number from 1 to 10')
 number = randint(1,10)
 for _ in range(10000000):
-    gues = int(input('Make your gues :'))
+    gues = int(input('Make your gues: '))
     if gues < number: print('The number is larger')
     if gues > number: print('The number is smaller')
     if gues == int(number):
